2456974990e620d321c7a45ad2e2f1f08b1d6d45
Matthew Elliott <46054623+Matt-S-Elliott@users.noreply.github.com>
2019-04-09 11:06:58 -0700

    Removed extra characters
    Removed random crap added by github for no good reason. Screw you, github.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -65,13 +65,11 @@ module.exports = app => {
     req.logout();
     res.redirect("/");
   });
-<<<<<<< HEAD
   
   app.post("/api/layout", isAuthenticated, (req, res) => {
     res.json(req.body);
   });
-=======
-  // END OF PREBUILT CODE
+
 
   app.post("/api/settings", isAuthenticated, (req, res) => {
     // console.log(req.body.data);
@@ -90,5 +88,4 @@ module.exports = app => {
 
   })
 
->>>>>>> 59c5a18ee2e8e196e08f67a610d1f2ef104e0c8a
 };
